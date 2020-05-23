42f5297c50c3ddbb867ea44b0692f8eb5696a4a5
Ciarra Murphy <mciarra@ymail.com>
2020-05-23 15:56:11 -0500

    added carousel element

--- script.js
+++ script.js
@@ -5,20 +5,41 @@ function postForm(){
         var req = new XMLHttpRequest();
         var printInfo = {name:document.getElementById("userName").value, email:document.getElementById("userEmail").value, 
         originallyFrom:document.getElementById("userOrigin").value, why:document.getElementById("userQuestion").value};
-        //printInfo.name = document.getElementById("nameForm").value;
-        //printInfo.age = document.getElementById("ageForm").value;
-        //printInfo.job = document.getElementById("jobForm").value;
-        //printInfo.major = document.getElementById("majorForm").value;
 
         req.open('POST', 'http://httpbin.org/post', true);
         req.setRequestHeader('Content-Type', 'application/json');
 
         console.log(JSON.stringify(printInfo));
         req.send(JSON.stringify(printInfo));
-        //console.log(JSON.parse(req.resonseText));
 
         event.preventDefault();
     });
 }
 
-console.log("In the post JS file!");
+// for the carousel animation
+
+var btnIndex = 1;
+movePics(btnIndex);
+
+function moveBtns(p){
+    movePics(btnIndex += p);
+}
+
+function movePics(p){
+    //document.getElementById("carouselBtn").addEventListener('click', function(event){
+    var i;
+    var slideShow = document.getElementsByClassName("carouselPics");
+    if (p > slideShow.length){
+        btnIndex = 1
+    }
+
+    if (p < 1){
+        btnIndex = slideShow.length
+    };
+
+    for (i = 0; i < slideShow.length; i++){
+        slideShow[i].style.display = "none";
+    }
+    slideShow[btnIndex-1].style.display = "block";
+    //});
+}
